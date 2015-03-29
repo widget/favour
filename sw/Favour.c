98d2cc65738a35bd6d7f98323867451cd4bae03d
Widget <widget@stdin.co.uk>
2015-03-29 21:42:58 +0100

    Try to deal with more usb-battery interactions, turns out our detect pin isn't detecting which power we're using, just what's present.  Tidy code, deal wih running off a USB charger instead of a computer.  Lower clock on battery

--- sw/Favour.c
+++ sw/Favour.c
@@ -26,9 +26,17 @@ USB_ClassInfo_HID_Device_t Keyboard_HID_Interface =
     },
 }; 
 
-// TODO change to 31
-#define LED_MAX_BRIGHT 16
-#define LED_FREQ 248
+#define soft_reset() \
+do \
+{ \
+wdt_enable(WDTO_15MS); \
+for(;;) \
+{ \
+} \
+} while(0)
+
+#define LED_MAX_BRIGHT 32
+#define LED_FREQ 256
 // Precalculate LED values, rather than in the interrupt
 uint8_t led_pwm[LED_MAX_BRIGHT];
 uint8_t count = 0;
@@ -45,7 +53,7 @@ ISR(TIMER0_COMPA_vect, ISR_BLOCK)
         count = 0;
         fcount++;
     }
-    if (fcount >= (LED_FREQ >> 5))
+    if (fcount >= (LED_FREQ >> 6))
     {
         fcount = 0;
         ticks++;
@@ -68,10 +76,26 @@ void set_led_pwm(const uint8_t led, const uint8_t bright)
 }
 
 // default to zero, save ram, the AVR way
-bool usb_present = true, hwb_last;
+bool vusb_present = true, hwb_last;
+bool hid_present = false;
 uint16_t last_press;
 bool toggle;
 uint8_t pattern_count;
+
+uint8_t set_pattern_get_next(pattern_t *pt, uint8_t idx)
+{
+    if (vusb_present)
+    {
+        memcpy_P(pt, PATTERNS_HI+idx, sizeof(pattern_t));
+        return (idx + 1) % NUM_PATTERNS_HI;
+    }
+    else
+    {
+        memcpy_P(pt, PATTERNS_LO+idx, sizeof(pattern_t));
+        return (idx + 1) % NUM_PATTERNS_LO;
+    }
+
+}
     
 /** Main program entry point. This routine contains the overall program flow, including initial
  *  setup of all components and the main program loop.
@@ -84,16 +108,10 @@ int main(void)
     SetupHardware();
 
     pattern_t current_pattern;
-    if (usb_present)
-    {
-        memcpy_P(&current_pattern, PATTERNS_HI, sizeof(pattern_t));
-    }
-    else
-    {
-        memcpy_P(&current_pattern, PATTERNS_LO, sizeof(pattern_t));
-    }
+    pattern_idx = set_pattern_get_next(&current_pattern, pattern_idx);
     GlobalInterruptEnable();
-    for (;;)
+    
+    while(1)
     {
         // Actually running through the selected LED pattern here
         if (ticks >= next)
@@ -120,38 +138,42 @@ int main(void)
             }
         }
         
-        if (usb_present)
+        if (vusb_present)
         {
+            // HID can't be detected if we don't do HID stuff
             HID_Device_USBTask(&Keyboard_HID_Interface);
             USB_USBTask();
-            
-            // Cycle onto the next pattern if caps is toggled and numlock is off
+        }
+        
+        if (hid_present)
+        {
+            // Cycle onto the next pattern if caps is toggled
             if (toggle)
             {
                 // Next pattern
-                pattern_idx = (pattern_idx + 1) % NUM_PATTERNS_HI;
-                // Copy it in
-                memcpy_P(&current_pattern, PATTERNS_HI+pattern_idx, sizeof(pattern_t));
+                pattern_idx = set_pattern_get_next(&current_pattern, pattern_idx);
                 // And restart
                 idx = 0;
-                
+                next = 0;
                 toggle = false;
             }
         }
         else
         {
-            if (pattern_count == 150)
+            if ((pattern_count == 150) || toggle)
             {
-                pattern_idx = (pattern_idx + 1) % NUM_PATTERNS_LO;
-                memcpy_P(&current_pattern, PATTERNS_LO+pattern_idx, sizeof(pattern_t));
-                idx = 0;
-                pattern_count = 0;
+                pattern_idx = set_pattern_get_next(&current_pattern, pattern_idx);
+                
+                idx = 0;                // Start at the beginning of the cycle
+                pattern_count = 0;      // Start the timer again
+                next = 0;               // Trigger cycle to start now
+                
+                toggle = false;
             }
             sei();
             sleep_cpu();
         }
         
-        // TODO Check if HWB is pressed, and cycle pattern based on it
         if (Buttons_GetStatus() & BUTTONS_HWB)
         {
             // check it was unpressed last cycle
@@ -168,11 +190,13 @@ int main(void)
             hwb_last = false;
         }
         
-        // Check power hasn't changed
-        bool new_usb_present = !(Buttons_GetStatus() & BUTTONS_VCC);
-        if (usb_present != new_usb_present)
+        // Check power hasn't changed.  But VUSB doesn't tell us if we're running
+        // on VUSB, just that it's present!
+        bool new_vusb_present = !(Buttons_GetStatus() & BUTTONS_VCC);
+        if (vusb_present != new_vusb_present)
         {
             // RESET
+            soft_reset();
         }
 	}
 }
@@ -195,52 +219,54 @@ void SetupHardware()
     
     // Check if we're on USB or battery,
     // BUTTONS_VCC will return true when VCC == 3V3
-    usb_present = !(Buttons_GetStatus() & BUTTONS_VCC);
+    vusb_present = !(Buttons_GetStatus() & BUTTONS_VCC);
     
-    LEDs_Init(usb_present);
+    LEDs_Init(vusb_present);
     
     TCNT0  = 0x00; // start value
     TIFR0 = 0; // clear interrupt flags just in case
     TIMSK0 = _BV(OCIE0A); // Interrupt enable on compare A
     TCCR0A = 0x02; // Clear Timer on Compare (CTC) mode
-        
-    if (usb_present)
+    
+    // Never using these, may as well leave them off
+    power_spi_disable();
+    power_timer1_disable();
+    power_usart1_disable();
+    // disable analogue comparator
+    ACSR |= _BV(ACD);
+    
+    // Debug is disabled in fuses, but BOD isn't
+    
+    if (bit_is_set(CLKSEL0,CLKS))
+    {
+        // Turn off RC osc too
+        CLKSEL0 &= ~(1 << RCE);
+    }
+    
+    if (vusb_present)
     {
         /* Disable clock division */
         clock_prescale_set(clock_div_1);
         USB_Init();
         
-        OCR0A  =   33; // 4.96kHz
+        OCR0A  =   29; // 4096Hz
         // set timer0 to be 16MHz/64 = 8andabit kHz
         TCCR0B = 0x03;
     }
     else
     {
-        // once tested, try 128
-        clock_prescale_set(clock_div_8);
+        // going slower than this leaves too little time for the interrupt
+        clock_prescale_set(clock_div_16);
         
         USB_Disable();
         
         power_usb_disable();
-        power_spi_disable();
-        power_timer1_disable();
-        power_usart1_disable();
-        // disable analogue comparator
-        ACSR |= _BV(ACD);
-        
-        // TODO we need to disable BOD and debug fuses
-        
-        if (bit_is_set(CLKSEL0,CLKS))
-        {
-            // Turn off RC osc too
-            CLKSEL0 &= ~(1 << RCE);
-        }
         
         // this should be the default
         set_sleep_mode(SLEEP_MODE_IDLE);
         sleep_enable();
         
-        OCR0A  =   249; // 400Hz
+        OCR0A  =   123; // 4096Hz
         // Set timer0 to be 8/(8*1) - 125kHz
         TCCR0B = 0x01;
     }
@@ -250,13 +276,11 @@ void SetupHardware()
 /** Event handler for the library USB Connection event. */
 void EVENT_USB_Device_Connect(void)
 {
-    //LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
 }
 
 /** Event handler for the library USB Disconnection event. */
 void EVENT_USB_Device_Disconnect(void)
 {
-    //LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
 }
 
 /** Event handler for the library USB Configuration Changed event. */
@@ -268,7 +292,7 @@ void EVENT_USB_Device_ConfigurationChanged(void)
 
     USB_Device_EnableSOFEvents();
 
-    //LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
+    hid_present = true;
 }
 
 /** Event handler for the library USB Control Request reception event. */
@@ -299,13 +323,13 @@ bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDIn
                                          void* ReportData,
                                          uint16_t* const ReportSize)
 {
+/*
     USB_KeyboardReport_Data_t* KeyboardReport = (USB_KeyboardReport_Data_t*)ReportData;
 
     uint8_t ButtonStatus_LCL = Buttons_GetStatus();
-
     uint8_t UsedKeyCodes = 0;
 
-    /*if (ButtonStatus_LCL & BUTTONS_HWB)
+    if (ButtonStatus_LCL & BUTTONS_HWB)
         KeyboardReport->KeyCode[UsedKeyCodes++] = HID_KEYBOARD_SC_F;
 
     if (UsedKeyCodes)
